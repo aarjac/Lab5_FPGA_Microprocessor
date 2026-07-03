3888e25bdb3b1f4bb57f47ba1c818c3a16804c79
aarjac <56480900+aarjac@users.noreply.github.com>
2026-07-03 13:18:00 -0400

    Write controller module

--- lab5.sv
+++ lab5.sv
@@ -13,6 +13,8 @@ end
 endmodule
 
 //4:1 MUX
+//instatiation temp of MUX 
+// MUX4to1 #(#) MUX# (.A(), .B(), .C(), .D(), .select(), .Y());
 module MUX4to1 #(parameter N = 8)(input [(N-1):0] A, B, C, D,
 input [1:0] select,
 output logic [(N-1):0] Y);
@@ -62,7 +64,7 @@ endmodule
 //needs editing
 //16 8-bit DReg
 module RegFile(input reset, clk, input [3:0] OPCODE, RA, RB, RD, 
-input [1:0] current_state, input [7:0] RF_data_in,
+input [1:0] state, input [7:0] RF_data_in,
 output logic [7:0] RF_data_out0, RF_data_out1);
 //16 registers, 8 bits each
 logic [7:0] RF [15:0];
@@ -81,7 +83,7 @@ always_ff @(posedge clk or posedge reset) begin
     else begin
         RF_data_out0 <= RF[RA];
         RF_data_out1 <= RF[RB];
-        if ((current_state == RWB) && 
+        if ((state == RWB) && 
         ((OPCODE != CMPJ) || (OPCODE != JMP) || (OPCODE != HALT))) 
             RF[RD] <= RF_data_in;
     end
@@ -89,7 +91,51 @@ end
 endmodule
 
 //controller
-module ControlUnit ();
+//implemented via FSM using DReg and MUX4to1
+//might have to fix some things here
+module ControlUnit (input clk, reset, input [3:0] OPCODE, RA, RB, RD,
+input [7:0] PC,
+output logic [1:0] state, 
+output logic [3:0] ALU_control,
+output logic MEM_write);
+//states
+localparam IF = 2'b00, FD = 2'b01, EX = 2'b10, RWB = 2'b11;
+//OPCODE checks
+localparam CMPJ = 4'b1101, JMP = 4'b1110, HALT = 4'b1111;
+//local variables
+logic [1:0] next_state;
+logic [7:0] next_PC;
+Dreg #(2) D1 (.clk(clk), .reset(reset), .D(next_state), 
+.Q(state));
+MUX4to1 #(2) MUX1 (.A(FD), .B(EX), .C(RWB), .D(IF), .select(state), 
+.Y(next_state));
+//determine next_state
+always_comb begin
+    //init values
+    ALU_control = 4'b0000; next_PC = PC; MEM_write = 1'b0;
+    case (state)
+        EX : begin
+            ALU_control = OPCODE;
+            next_PC = PC;
+        end
+        RWB : begin
+            //mabye change ALU_control value here
+            ALU_control = OPCODE;
+            MEM_write = 1'b1;
+            case (OPCODE)
+                CMPJ : begin
+                    if (RA >= RB)
+                        next_PC = PC + RD;
+                    else
+                        next_PC = PC;
+                end
+                JMP : next_PC = {RA,RB};
+                HALT : next_PC = PC;
+                default : next_PC = PC;
+            endcase
+        end
+    endcase
+end
 endmodule
 
 //ALU
