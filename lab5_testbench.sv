12b6d986d228d4867fda3e86d17a68c64c0a75a1
aarjac <56480900+aarjac@users.noreply.github.com>
2026-07-04 12:56:27 -0400

    Create testbench for simulation

--- lab5_testbench.sv
+++ lab5_testbench.sv
@@ -1,1 +1,17 @@
+`timescale 1ns/1ps
 
+//simulation 
+module lab5_testbench();
+//inputs
+logic clk, reset;
+//outputs
+logic Cout, OF;
+logic [1:0] state;
+logic [3:0] OPCODE;
+logic [7:0] PC, ALU_out, W_Reg;
+lab5 L1 (.clk(), .reset(), 
+.OPCODE(), .state(), .PC(),
+.ALU_out(), .W_Reg(), .Cout(), .OF());
+initial begin
+end
+endmodule
